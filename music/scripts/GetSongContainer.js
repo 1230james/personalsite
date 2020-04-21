9df9cc49244827e88347237f02bc910004cd725d
James Hyun <jameshyun1230@gmail.com>
2020-04-21 19:51:49 -0400

    Tweaked CGI code to fix a few issues
    - Fixed View Info button being in the wrong place
    - Changed all the apostrophes to quotation marks inside of the strings (code looks more ugly now but songs with apostrophes in their titles won't cause issues now)
    - Changed the songContainer id properties and the table of contents anchors to remove whitespace for HTML compliance

--- music/scripts/GetSongContainer.js
+++ music/scripts/GetSongContainer.js
@@ -15,15 +15,15 @@ Template for one entry in musicList
     }
 
 Sample of intended HTML for each song container
-    <div class="songContainer">
+    <div id="nameNoWhitespace" class="songContainer">
         <div class="song">
-            <img src="icon" alt="name"></img>
+            <img src="icon" alt="name"/>
             <h3 class="textcenter">name</h3>
             <h4 class="textcenter">origin</h4>
-            <p>desc</p>
-        </div>
-        <div class="mx-auto textcenter">
-            <a class="btn btn-primary" href="/music">View</a>
+            <p class="textcenter">desc</p>
+            <div class="mx-auto textcenter">
+                <a class="btn btn-primary" href="/music">View</a>
+            </div>
         </div>
     </div>
 */
@@ -33,21 +33,20 @@ Sample of intended HTML for each song container
 // The idea: We're gonna start from the inside (i.e. deepest layer) and then
 // work our way outwards to make the string concatenation look less disgusting.
 module.exports = function(obj) {
-    // the "song" container; contains image, name, etc.
-    let iconImg = "<img src='" + obj.icon + "' alt='" + obj.name + "'/>";
-    let header = "<h3 class='textcenter'>" + obj.name + "</h3>";
-    let subheader = "<h4 class='textcenter'>" + obj.origin + "</h4>";
-    let desc = "<p class='textcenter'>" + obj.desc + "</p>";
-    let songDiv = "<div class='song'>" + iconImg + header + subheader + desc
-                + "</div>";
-    
     // the button container with the View Info button
-    let buttonDiv = "<div class='mx-auto textcenter'>" // I'm about to do something mildly stupid
-        + "<a class='btn btn-primary' href='"+ obj.path + "'>View Info</a>"
+    let buttonDiv = "<div class=\"mx-auto textcenter\">" // I\"m about to do something mildly stupid
+        + "<a class=\"btn btn-primary\" href=\""+ obj.path + "\">View Info</a>"
         + "</div>";
-        
-    // the entire songContainer itself
-    let songContainer = "<div class='songContainer'>" + songDiv + buttonDiv + "</div>";
     
-    return songContainer
+    // the "song" container; contains image, name, etc.
+    let iconImg = "<img src=\"" + obj.icon + "\" alt=\"" + obj.name + "\"/>";
+    let header = "<h3 class=\"textcenter\">" + obj.name + "</h3>";
+    let subheader = "<h4 class=\"textcenter\">" + obj.origin + "</h4>";
+    let desc = "<p class=\"textcenter\">" + obj.desc + "</p>";
+    let songDiv = "<div class=\"song\">" + iconImg + header + subheader + desc
+                + buttonDiv + "</div>";
+        
+    // return the final product
+    return "<div id=\"" + obj.name.split(" ").join("") + // TIL V8 can split and join arrays pretty fast
+        "\" class=\"songContainer\">" + songDiv + "</div>";
 }
--- music/scripts/GetSongListItem.js
+++ music/scripts/GetSongListItem.js
@@ -12,12 +12,18 @@ Template for one entry in musicList
         path: "path/to/page",
         completed: boolean
     }
+    
+Snippet of sample of intended HTML for each song container
+    <div id="nameNoWhitespace" class="songContainer">
+        <!-- See GetSongContainer.js for the full sample -->
+    </div>
 */
 
 // =============================================================================
 
-// This is approaching useless encapsulation territory, but I'll try to justify
+// This is approaching useless encapsulation territory, but I\"ll try to justify
 // to myself it with code readability.
 module.exports = function(obj) {
-    return "<li><a href='#" + obj.name + "'>" + obj.name + "</a></li>";
+    return "<li><a href=\"#" + obj.name.split(" ").join("") + "\">" + obj.name +
+        "</a></li>";
 }
