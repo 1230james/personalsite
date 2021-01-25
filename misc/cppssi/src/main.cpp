188c57eb93357a60c5cbca1bfc0a16fd437d1c34
James Hyun <jameshyun1230@gmail.com>
2021-01-25 16:44:57 +0000

    C++ SSI
    this isnt okay

---
+++ misc/cppssi/src/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <cstdlib>
+
+int main() {
+    printf("Content-type: text/html"
+           "</span>"
+           "<h2 class='big textcenter'>C/C++ SSI Test</h2>"
+           "<p class='textcenter'>Holy fuzzy, this is running in C++"
+           "<br/><br/>Time for some <i>HIGH SPEED</i> dynamic pages bro!!!"
+           "<br/><small><small>oh god have i gone insane</small></small></p>"
+           "<span style='display:none;'>"
+           );
+    return EXIT_SUCCESS;
+}
